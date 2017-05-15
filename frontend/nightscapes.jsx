208f2661c8137fbe419b5f4802a974ad4a854478
Christine <christinegaribian@gmail.com>
2017-05-14 20:54:28 -0700

    Finish Session API Utils

--- frontend/nightscapes.jsx
+++ frontend/nightscapes.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import * as APIUtil from './util/session_api_util';
+window.APIUtil = APIUtil;
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
     ReactDOM.render(<h1>Welcome to Nightscapes</h1>, root);
