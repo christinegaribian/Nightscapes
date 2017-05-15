0f7e8ea9ffa7e6e8cef06ebdc8018da5de274d2d
Christine <christinegaribian@gmail.com>
2017-05-14 21:44:48 -0700

    Install redux-thunk

--- frontend/nightscapes.jsx
+++ frontend/nightscapes.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+
+
 import * as APIUtil from './util/session_api_util';
 window.APIUtil = APIUtil;
+
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
+    const store = configureStore();
+    window.store = store;
     ReactDOM.render(<h1>Welcome to Nightscapes</h1>, root);
 });
