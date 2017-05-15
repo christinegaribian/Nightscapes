c2f2aeb66325c65d607d9dfb5f1507614cc831ea
Christine <christinegaribian@gmail.com>
2017-05-14 19:36:16 -0700

    Write Sessions Controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -12,9 +12,10 @@ class Api::UsersController < ApplicationController
 
   def show
     @user = User.find_by_credentials(
-                params[:user][:username],
-                params[:user][:password]
-                )
+      params[:user][:username],
+      params[:user][:password]
+    )
+
     if @user
       render 'api/users/show'
     else
