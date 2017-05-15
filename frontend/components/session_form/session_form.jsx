d8fd00060dfcadee0fb8ae12be31bfa1b80c6644
Christine <christinegaribian@gmail.com>
2017-05-15 10:21:06 -0700

    added conditional to rendering of errors in session_form

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -3,14 +3,14 @@ import { Link, withRouter } from 'react-router-dom';
 
 
 class SessionForm extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			username: "",
-			password: "",
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
       first_name: "",
       last_name: ""
-		};
+    };
 
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -28,57 +28,59 @@ class SessionForm extends React.Component {
   }
 
   renderErrors() {
-    return(
-      <ul>
-        {this.props.errors.map((error, i) => (
-          <li key={`error-${i}`}>
-            {error}
-          </li>
-        ))}
-      </ul>
-    );
+    if (this.props.errors){
+      return(
+        <ul>
+          {this.props.errors.map((error, i) => (
+            <li key={`error-${i}`}>
+              {error}
+            </li>
+          ))}
+        </ul>
+      );  
+    }
   }
 
   navLink() {
-      if (this.props.formType === 'login') {
-        return <Link to="/signup">sign up instead</Link>;
+    if (this.props.formType === 'login') {
+      return <Link to="/signup">sign up instead</Link>;
       } else {
         return <Link to="/login">log in instead</Link>;
+        }
+      }
+
+      render() {
+        return (
+          <div className="login-form-container">
+            <form onSubmit={this.handleSubmit}>
+              Welcome to Nightscapes!
+              <br/>
+              Please {this.props.formType} or {this.navLink()}
+              {this.renderErrors()}
+              <div className="login-form">
+                <br/>
+                <label>Username:
+                  <input type="text"
+                    value={this.state.username}
+                    onChange={this.update('username')}
+                    className="login-input"
+                    />
+                </label>
+                <br/>
+                <label>Password:
+                  <input type="password"
+                    value={this.state.password}
+                    onChange={this.update('password')}
+                    className="login-input"
+                    />
+                </label>
+                <br/>
+                <input type="submit" value="Submit" />
+              </div>
+            </form>
+          </div>
+        );
       }
     }
 
-  render() {
-    return (
-      <div className="login-form-container">
-        <form onSubmit={this.handleSubmit}>
-          Welcome to Nightscapes!
-          <br/>
-          Please {this.props.formType} or {this.navLink()}
-          {this.renderErrors()}
-          <div className="login-form">
-            <br/>
-            <label>Username:
-              <input type="text"
-                value={this.state.username}
-                onChange={this.update('username')}
-                className="login-input"
-              />
-            </label>
-            <br/>
-            <label>Password:
-              <input type="password"
-                value={this.state.password}
-                onChange={this.update('password')}
-                className="login-input"
-              />
-            </label>
-            <br/>
-            <input type="submit" value="Submit" />
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
-
-export default withRouter(SessionForm);
+    export default withRouter(SessionForm);
--- frontend/nightscapes.jsx
+++ frontend/nightscapes.jsx
@@ -5,8 +5,19 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
 
+import * as APIUtil from './util/session_api_util';
+window.APIUtil = APIUtil;
 document.addEventListener('DOMContentLoaded', () => {
-    const root = document.getElementById('root');
-    const store = configureStore();
-    ReactDOM.render(<Root store={store}/>, root);
+  let store;
+  if (window.currentUser) {
+    const preloadedState = { session: { currentUser: window.currentUser } };
+    store = configureStore(preloadedState);
+    // delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
+
+  window.store = store;
+  const root = document.getElementById('root');
+  ReactDOM.render(<Root store={store} />, root);
 });
