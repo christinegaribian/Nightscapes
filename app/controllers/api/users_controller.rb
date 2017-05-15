25debd27d9bbdb3b5baf3a69fc73f2b851c37d9b
Christine <christinegaribian@gmail.com>
2017-05-14 19:31:54 -0700

    Write UsersController

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,7 +1,29 @@
 class Api::UsersController < ApplicationController
   def create
+    @user = User.new(user_params)
+    if @user.save
+      login(@user)
+      render "api/users/show"
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
   end
 
-  def update
+
+  def show
+    @user = User.find_by_credentials(
+                params[:user][:username],
+                params[:user][:password]
+                )
+    if @user
+      render 'api/users/show'
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password, :first_name, :last_name)
   end
 end
