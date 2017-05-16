e41e299f451ff9fd45bd6510ddeffc2dc701034f
Christine <christinegaribian@gmail.com>
2017-05-16 15:49:48 -0700

    F ix migration typo

--- db/migrate/20170515015811_create_users.rb
+++ db/migrate/20170515015811_create_users.rb
@@ -4,8 +4,8 @@ class CreateUsers < ActiveRecord::Migration[5.0]
       t.string :username, null: false, unique: true
       t.string :session_token, null: false, unique: true
       t.string :password_digest, null: false
-      t.string :first_name, null: false
-      t.string :last_name, null: false
+      t.string :first_name
+      t.string :last_name
       t.integer :views, default: 0
       t.integer :photo_count, default: 0
       t.integer :follower_count, default: 0
--- db/migrate/20170515181136_remove_photo_count_follower_countfrom_users.rb
+++ db/migrate/20170515181136_remove_photo_count_follower_countfrom_users.rb
@@ -1,6 +1,12 @@
 class RemovePhotoCountFollowerCountfromUsers < ActiveRecord::Migration[5.0]
-  def change
+
+  def up
     remove_column :users, :photo_count
     remove_column :users, :follower_count
+  end
+
+  def down
+    add_column :users, :photo_count, :integer
+    add_column :users, :follower_count, :integer
   end
 end
