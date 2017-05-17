0a0ef230d49125e8d9e7796043648defc84499d7
Christine <christinegaribian@gmail.com>
2017-05-17 14:38:47 -0700

    Remove unneccessary import

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import SessionFormContainer from './session_form/session_form_container';
 import { AuthRoute, ProtectedRoute } from '../util/route_util.jsx'
-import GreetingContainer from './greeting/greeting_container';
 // Can add greeting container back in later as nav bar
 import HomeContainer from './home/home_container';
 import UserPageContainer from './user_page/user_page_container';
