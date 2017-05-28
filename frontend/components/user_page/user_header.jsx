f93f4b3fbf37b7a70d30f2a58debd20d11a5712a
Christine <christinegaribian@gmail.com>
2017-05-28 00:10:27 -0700

    Add alt tag to indicate that user profile picture leads to their personal site

--- frontend/components/user_page/user_header.jsx
+++ frontend/components/user_page/user_header.jsx
@@ -14,7 +14,7 @@ class UserHeader extends React.Component {
     }
     return (
       <div className="user-header">
-        <a href={user.user_site_url} target="_blank"><img className="user-header-avatar"
+        <a href={user.user_site_url} target="_blank" title="Personal Site"><img className="user-header-avatar"
           src={user.user_img_url}
           /></a>
         <div className="user-header-info">
